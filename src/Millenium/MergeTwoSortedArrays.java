fa057ce67675b64f3530bf8b5f0643a5d845a107
Rajesh Patel <rpatel@ondeck.com>
2017-06-29 16:30:40 -0400

    added links

---
+++ src/Millenium/MergeTwoSortedArrays.java
@@ -0,0 +1,13 @@
+package Millenium;
+
+/**
+ * Given two sorted arrays a and b such that a.length = 2 * b.length, but both have b.length elements
+ * filled in sorted.  Fill in array a to merge both array into one, resulting in one sorted array.
+ *
+ * a = [4,5,6,0,0,0]
+ * b = [1,2,3]
+ * then a = [1,2,3,4,5,6]
+ *
+ */
+public class MergeTwoSortedArrays {
+}
